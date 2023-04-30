645997aed8360837b145a2ae6adaf4c58c621863
Kushagra Gupta <kushagragupta01@hera3.ece.tamu.edu>
2023-04-29 23:55:19 -0500

    added UVM_NOCOMPARE in multiple fields in system bus monitor

--- project/uvm/system_bus_monitor_c.sv
+++ project/uvm/system_bus_monitor_c.sv
@@ -96,41 +96,11 @@ class system_bus_monitor_c extends uvm_monitor;
            // if (vi_sbus_if.ica === 1'b1) //no idea about icache
            //     s_packet.bus_req_type = INVALIDATE; bus_req_snoop 
 
-            
-			/*
-                if (vi_sbus_if.bus_lv1_lv2_req_snoop == 2'd0)
-                    s_packet.bus_req_snoop = 4'd0; 
-                
-                if (vi_sbus_if.bus_lv1_lv2_req_snoop == 2'd1)
-                    s_packet.bus_req_snoop = 4'd1; 
-
-                if (vi_sbus_if.bus_lv1_lv2_req_snoop == 2'd2)
-                    s_packet.bus_req_snoop = 4'd2; 
-
-                if (vi_sbus_if.bus_lv1_lv2_req_snoop == 2'd3)
-                    s_packet.bus_req_snoop = 4'd3; 
-
-			*/
-
+	
             // trigger point for creating the packet
             @(posedge (|vi_sbus_if.bus_lv1_lv2_gnt_proc));
             `uvm_info(get_type_name(), "Packet creation triggered", UVM_LOW)
             s_packet = sbus_packet_c::type_id::create("s_packet", this);
-
-
-			// Snoop req processors
-           	s_packet.bus_req_snoop = 4'h0; 
-            if(vi_sbus_if.bus_lv1_lv2_req_snoop[0] == 1'b1)
-           		s_packet.bus_req_snoop[0] = 1'b1; 
-				
-            if(vi_sbus_if.bus_lv1_lv2_req_snoop[1] == 1'b1)
-           		s_packet.bus_req_snoop[1] = 1'b1; 
-
-            if(vi_sbus_if.bus_lv1_lv2_req_snoop[2] == 1'b1)
-           		s_packet.bus_req_snoop[2] = 1'b1; 
-
-            if(vi_sbus_if.bus_lv1_lv2_req_snoop[3] == 1'b1)
-           		s_packet.bus_req_snoop[3] = 1'b1; 
 
 
 
@@ -151,15 +121,17 @@ class system_bus_monitor_c extends uvm_monitor;
             // bus request type 
             if (vi_sbus_if.bus_rd === 1'b1)
                 s_packet.bus_req_type = BUS_RD;
-            // bus request type:
-            //if (vi_sbus_if.bus_rd == 1'b1)
-            //    s_packet.bus_req_type = BUS_RD; 
-
+            
             if (vi_sbus_if.bus_rdx === 1'b1)
                 s_packet.bus_req_type = BUS_RDX; 
             
             if (vi_sbus_if.invalidate === 1'b1)
                 s_packet.bus_req_type = INVALIDATE; 
+
+
+
+
+            `uvm_info(get_type_name(), $sformatf("Received SBUS Packet: %d, gen:%d", vi_sbus_if.bus_lv1_lv2_req_snoop, s_packet.bus_req_snoop),UVM_LOW)
 
             // proc which requested the bus access
             case (1'b1)
@@ -172,6 +144,7 @@ class system_bus_monitor_c extends uvm_monitor;
             // address requested
             s_packet.req_address = vi_sbus_if.addr_bus_lv1_lv2;
 
+		    
             // fork and call tasks
             fork: update_info
 
@@ -190,6 +163,26 @@ class system_bus_monitor_c extends uvm_monitor;
                             vi_sbus_if.bus_lv1_lv2_gnt_snoop[3]: s_packet.req_serviced_by = SERV_SNOOP3;
                             vi_sbus_if.bus_lv1_lv2_gnt_lv2     : s_packet.req_serviced_by = SERV_L2;
                         endcase
+
+						// Check for the snooping reqs:	
+            			//@(posedge (|vi_sbus_if.bus_lv1_lv2_gnt_snoop));
+            			s_packet.bus_req_snoop = vi_sbus_if.bus_lv1_lv2_req_snoop; 
+
+						/*
+            			if (vi_sbus_if.bus_lv1_lv2_req_snoop[0] === 1'b1)
+           					s_packet.bus_req_snoop[0] = 1'b1; 
+							
+            			if (vi_sbus_if.bus_lv1_lv2_req_snoop[1] === 1'b1)
+           					s_packet.bus_req_snoop[1] = 1'b1; 
+
+            			if (vi_sbus_if.bus_lv1_lv2_req_snoop[2] === 1'b1)
+           					s_packet.bus_req_snoop[2] = 1'b1; 
+
+            			if (vi_sbus_if.bus_lv1_lv2_req_snoop[3] === 1'b1)
+           					s_packet.bus_req_snoop[3] = 1'b1; 
+						*/
+
+
                     end
 					
                 end: req_service_check
